d634a2406b708553566211b7fd346daa7a0e25bf
ecotner <2.71828cotner@gmail.com>
2020-06-14 18:39:06 -0400

    added more options in training method

--- code/models.py
+++ code/models.py
@@ -2,7 +2,8 @@ import tqdm
 import torch.tensor as tt
 import pyro
 from pyro.infer import SVI, Trace_ELBO
-from pyro.infer.autoguide import AutoMultivariateNormal, init_to_mean
+import pyro.infer.autoguide
+from pyro.infer.autoguide import AutoMultivariateNormal, AutoDiagonalNormal, init_to_mean,  AutoLaplaceApproximation
 from pyro.optim import Adam
 
 class RegressionBase:
@@ -17,11 +18,20 @@ class RegressionBase:
     def __call__(self):
         raise NotImplementedError
         
-    def train(self, num_steps, lr=1e-2):
-        pyro.clear_param_store()
-        self.guide = AutoMultivariateNormal(self, init_loc_fn=init_to_mean)
+    def train(self, num_steps, lr=1e-2, restart=True, autoguide=None, use_tqdm=True):
+        if restart:
+            pyro.clear_param_store()
+            if autoguide is None:
+                autoguide = AutoMultivariateNormal
+            else:
+                autoguide = getattr(pyro.infer.autoguide, autoguide)
+            self.guide = autoguide(self, init_loc_fn=init_to_mean)
         svi = SVI(self, guide=self.guide, optim=Adam({"lr": lr}), loss=Trace_ELBO())
         loss = []
-        for _ in tqdm.notebook.tnrange(num_steps):
+        if use_tqdm:
+            iterator = tqdm.notebook.tnrange(num_steps)
+        else:
+            iterator = range(num_steps)
+        for _ in iterator:
             loss.append(svi.step())
         return loss
